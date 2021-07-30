d345595aca4850c29923c5ae2be10e53070a6bb1
Denis Pipko <pipko_work@mail.ru>
2021-07-30 15:43:26 +0300

    AOP functionality added, passing test time tracking with localization

--- testing-students-app/src/main/java/ru/pipko/otus/homework/App.java
+++ testing-students-app/src/main/java/ru/pipko/otus/homework/App.java
@@ -3,9 +3,11 @@ package ru.pipko.otus.homework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import ru.pipko.otus.homework.service.*;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class App {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
---
+++ testing-students-app/src/main/java/ru/pipko/otus/homework/aop/InterviewDurationAspect.java
@@ -0,0 +1,36 @@
+package ru.pipko.otus.homework.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import ru.pipko.otus.homework.service.LocalizationService;
+import ru.pipko.otus.homework.service.PrintService;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Component
+@Aspect
+public class InterviewDurationAspect {
+
+    private final LocalizationService localizationService;
+    private final PrintService printService;
+
+    public InterviewDurationAspect(LocalizationService localizationService, PrintService printService){
+
+        this.localizationService = localizationService;
+        this.printService = printService;
+    }
+
+
+    @Around("execution(* ru.pipko.otus.homework.service.*.askQuestions(..))")
+    public void timingInterviewAspect(ProceedingJoinPoint joinPoint) throws Throwable {
+        LocalDateTime dtStart = LocalDateTime.now();
+        joinPoint.proceed();
+        Duration duration = Duration.between(dtStart, LocalDateTime.now());
+        String message = localizationService.localizeMessage("strings.passing.duration",new String[] {String.valueOf(duration.getSeconds())});
+        printService.printLn(message);
+    }
+
+}
