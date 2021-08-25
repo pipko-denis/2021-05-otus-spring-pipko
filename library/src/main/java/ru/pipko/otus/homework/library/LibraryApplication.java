bc57ff91bc203b368ac5efb85c0fbfb612da3074
Denis Pipko <pipko_work@mail.ru>
2021-08-25 23:37:49 +0300

    Initial library commit

---
+++ library/src/main/java/ru/pipko/otus/homework/library/LibraryApplication.java
@@ -0,0 +1,13 @@
+package ru.pipko.otus.homework.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+}
