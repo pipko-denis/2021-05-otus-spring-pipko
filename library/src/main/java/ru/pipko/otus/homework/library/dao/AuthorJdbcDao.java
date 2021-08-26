d11846fcd84b2e4b6aae54352577af01a4f5bcbf
Denis Pipko <pipko_work@mail.ru>
2021-08-27 01:20:00 +0300

    AuthorJdbcDao getById completed

--- library/src/main/java/ru/pipko/otus/homework/library/dao/AuthorJdbcDao.java
+++ library/src/main/java/ru/pipko/otus/homework/library/dao/AuthorJdbcDao.java
@@ -1,13 +1,24 @@
 package ru.pipko.otus.homework.library.dao;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.RowCallbackHandler;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.stereotype.Repository;
 import ru.pipko.otus.homework.library.domain.Author;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
+@RequiredArgsConstructor
 public class AuthorJdbcDao implements AuthorDao{
+
+    private final NamedParameterJdbcOperations jdbc;
+
     @Override
     public List<Author> getAll() {
         return null;
@@ -15,7 +26,9 @@ public class AuthorJdbcDao implements AuthorDao{
 
     @Override
     public Optional<Author> getById(long id) {
-        return Optional.empty();
+        Map<String, Object> params = Map.of("id",id);
+        Author result  = jdbc.queryForObject("SELECT ID, NAME FROM AUTHORS WHERE id = :id ORDER BY NAME", params, new AuthorRowMapper());
+        return Optional.of(result) ;
     }
 
     @Override
@@ -31,5 +44,13 @@ public class AuthorJdbcDao implements AuthorDao{
     @Override
     public int delete(Author book) {
         return 0;
+    }
+
+    private class AuthorRowMapper implements RowMapper<Author> {
+
+        @Override
+        public Author mapRow(ResultSet resultSet, int i) throws SQLException {
+            return new Author(resultSet.getLong("id"),resultSet.getString("name"));
+        }
     }
 }
