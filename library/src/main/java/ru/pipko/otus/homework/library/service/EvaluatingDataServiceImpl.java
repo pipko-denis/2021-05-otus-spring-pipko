57d2d903ec78513b14b3e09f050a08b0f096a897
Denis Pipko <pipko_work@mail.ru>
2021-08-28 01:24:18 +0300

    Evaluating service now has correct implementation

--- library/src/main/java/ru/pipko/otus/homework/library/service/EvaluatingDataServiceImpl.java
+++ library/src/main/java/ru/pipko/otus/homework/library/service/EvaluatingDataServiceImpl.java
@@ -2,10 +2,20 @@ package ru.pipko.otus.homework.library.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.regex.Pattern;
+
 @Service
 public class EvaluatingDataServiceImpl implements EvaluatingDataService{
+
+    private final static Pattern patternPositiveInt = Pattern.compile("\\d+");
+
+
     @Override
     public boolean isThereAreOnlyDigitsInText(String text) {
-        return false;
+        if (text == null) {
+            return false;
+        }
+        return patternPositiveInt.matcher(text).matches();
     }
+
 }
