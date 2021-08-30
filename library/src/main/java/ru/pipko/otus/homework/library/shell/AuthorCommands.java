dea4dfaa29e46e6ccd10d968352d01c5aee15c9a
Denis Pipko <pipko_work@mail.ru>
2021-08-31 01:18:12 +0300

    Small modifications of AuthorCommands

--- library/src/main/java/ru/pipko/otus/homework/library/shell/AuthorCommands.java
+++ library/src/main/java/ru/pipko/otus/homework/library/shell/AuthorCommands.java
@@ -6,21 +6,17 @@ import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
 import ru.pipko.otus.homework.library.dao.AuthorDao;
 import ru.pipko.otus.homework.library.domain.Author;
-import ru.pipko.otus.homework.library.service.EvaluatingDataServiceImpl;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @ShellComponent
 public class AuthorCommands {
 
-
-    private final EvaluatingDataServiceImpl evaluatingDataService;
     private final AuthorDao authorDao;
 
-    @ShellMethod(value = "Adding author with all requisites", key = {"aa", "add-author"})
+    @ShellMethod(value = "Adding author command", key = {"aa", "add-author"})
     public String addAuthor(@ShellOption(value = {"name", "n"}) String name) {
 
         final Author author = new Author(name);
@@ -29,13 +25,13 @@ public class AuthorCommands {
         return recCount+" records added into Author table, new author id is " + author.getId() + ".";
     }
 
-    @ShellMethod(value = "Listing all authors", key = {"authors-list", "al"})
-    public String getBooksList() {
+    @ShellMethod(value = "Listing all authors command", key = {"authors-list", "al"})
+    public String getAuthorsList() {
 
         final List<Author> authorList = authorDao.getAll();
 
         final String result = "Authors list: \n" + authorList.stream().map(author ->
-                        "Author: " + author.getName())
+                        "Author "+author.getId()+": " + author.getName())
                 .collect(Collectors.joining("\n"));
 
         return result;
