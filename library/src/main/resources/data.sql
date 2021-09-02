e625318ccc271d874ed99716ef206ab38acfc217
Denis Pipko <pipko_work@mail.ru>
2021-09-02 23:12:24 +0300

    Small modification of data, names are modified in books etc.

--- library/src/main/resources/data.sql
+++ library/src/main/resources/data.sql
@@ -1,10 +1,10 @@
-INSERT INTO AUTHORS(id,name) VALUES(1, 'TestAuthor1');
-INSERT INTO AUTHORS(id,name) VALUES(2, 'TestAuthor2');
+INSERT INTO AUTHORS(id,name) VALUES(1, 'ProdAuthor1');
+INSERT INTO AUTHORS(id,name) VALUES(2, 'ProdAuthor2');
 
-INSERT INTO GENRES(id,name) VALUES(1, 'TestGenre1');
-INSERT INTO GENRES(id,name) VALUES(2, 'TestGenre2');
+INSERT INTO GENRES(id,name) VALUES(1, 'ProdGenre1');
+INSERT INTO GENRES(id,name) VALUES(2, 'ProdGenre2');
 
-INSERT INTO BOOKS(id,name,author_id, genre_id) VALUES(1, 'TestBook1',1,1);
-INSERT INTO BOOKS(id,name,author_id, genre_id) VALUES(2, 'TestBook2',1,2);
-INSERT INTO BOOKS(id,name,author_id, genre_id) VALUES(3, 'TestBook3',2,1);
-INSERT INTO BOOKS(id,name,author_id, genre_id) VALUES(4, 'TestBook4',2,2);
+INSERT INTO BOOKS(id,name,author_id, genre_id) VALUES(1, 'ProdBook1',1,1);
+INSERT INTO BOOKS(id,name,author_id, genre_id) VALUES(2, 'ProdBook2',1,2);
+INSERT INTO BOOKS(id,name,author_id, genre_id) VALUES(3, 'ProdBook3',2,1);
+INSERT INTO BOOKS(id,name,author_id, genre_id) VALUES(4, 'ProdBook4',2,2);
