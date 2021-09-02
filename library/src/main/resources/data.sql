149456c4120ee17959fa6261f967be6459a6b456
Denis Pipko <pipko_work@mail.ru>
2021-09-02 19:03:34 +0300

    Sql-script file with data for production modified

--- library/src/main/resources/data.sql
+++ library/src/main/resources/data.sql
@@ -1,10 +1,10 @@
-INSERT INTO AUTHORS(id,name) VALUES(1, 'Author1');
-INSERT INTO AUTHORS(id,name) VALUES(2, 'Author2');
+INSERT INTO AUTHORS(id,name) VALUES(1, 'TestAuthor1');
+INSERT INTO AUTHORS(id,name) VALUES(2, 'TestAuthor2');
 
-INSERT INTO GENRES(id,name) VALUES(1, 'Genre1');
-INSERT INTO GENRES(id,name) VALUES(2, 'Genre2');
+INSERT INTO GENRES(id,name) VALUES(1, 'TestGenre1');
+INSERT INTO GENRES(id,name) VALUES(2, 'TestGenre2');
 
-INSERT INTO BOOKS(id,name,author_id, genre_id) VALUES(1, 'Book1',1,1);
-INSERT INTO BOOKS(id,name,author_id, genre_id) VALUES(2, 'Book2',1,2);
-INSERT INTO BOOKS(id,name,author_id, genre_id) VALUES(3, 'Book3',2,1);
-INSERT INTO BOOKS(id,name,author_id, genre_id) VALUES(4, 'Book4',2,2);
+INSERT INTO BOOKS(id,name,author_id, genre_id) VALUES(1, 'TestBook1',1,1);
+INSERT INTO BOOKS(id,name,author_id, genre_id) VALUES(2, 'TestBook2',1,2);
+INSERT INTO BOOKS(id,name,author_id, genre_id) VALUES(3, 'TestBook3',2,1);
+INSERT INTO BOOKS(id,name,author_id, genre_id) VALUES(4, 'TestBook4',2,2);
