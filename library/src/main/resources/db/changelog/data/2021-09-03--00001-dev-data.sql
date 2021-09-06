2c049a56e4a0f33881eaf6cb0b644c98d3cc5064
Denis Pipko <pipko_work@mail.ru>
2021-09-06 23:14:18 +0300

    Liquibase migrations added

--- library/src/main/resources/db/changelog/data/2021-09-03--00001-dev-data.sql
+++ library/src/main/resources/db/changelog/data/2021-09-03--00001-dev-data.sql
@@ -1,9 +1,14 @@
+--liquibase formatted sql
+
+--changeset pipko:2021-09-03--00001-all-data context:dev
 INSERT INTO AUTHORS(id,name) VALUES(1, 'ProdAuthor1');
 INSERT INTO AUTHORS(id,name) VALUES(2, 'ProdAuthor2');
 
+--changeset pipko:2021-09-03--00002-all-data context:dev
 INSERT INTO GENRES(id,name) VALUES(1, 'ProdGenre1');
 INSERT INTO GENRES(id,name) VALUES(2, 'ProdGenre2');
 
+--changeset pipko:2021-09-03--00003-all-data context:dev
 INSERT INTO BOOKS(id,name,author_id, genre_id) VALUES(1, 'ProdBook1',1,1);
 INSERT INTO BOOKS(id,name,author_id, genre_id) VALUES(2, 'ProdBook2',1,2);
 INSERT INTO BOOKS(id,name,author_id, genre_id) VALUES(3, 'ProdBook3',2,1);
--- library/src/main/resources/db/changelog/data/2021-09-03--00001-test-data.sql
+++ library/src/main/resources/db/changelog/data/2021-09-03--00001-test-data.sql
@@ -1,9 +1,14 @@
+--liquibase formatted sql
+
+--changeset pipko:2021-09-03--00001-all-data context:test
 INSERT INTO AUTHORS(id,name) VALUES(1, 'Author1');
 INSERT INTO AUTHORS(id,name) VALUES(2, 'Author2');
 
+--changeset pipko:2021-09-03--00002-all-data context:test
 INSERT INTO GENRES(id,name) VALUES(1, 'Genre1');
 INSERT INTO GENRES(id,name) VALUES(2, 'Genre2');
 
+--changeset pipko:2021-09-03--00003-all-data context:test
 INSERT INTO BOOKS(id,name,author_id, genre_id) VALUES(1, 'Book1',1,1);
 INSERT INTO BOOKS(id,name,author_id, genre_id) VALUES(2, 'Book2',1,2);
 INSERT INTO BOOKS(id,name,author_id, genre_id) VALUES(3, 'Book3',2,1);
