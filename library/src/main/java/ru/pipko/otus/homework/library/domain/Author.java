4e9792ca2de170f190e9e9950fbecf226bb172b0
Denis Pipko <pipko_work@mail.ru>
2021-09-23 23:20:36 +0300

    Domain classes modified, since now they are jpa entities

--- library/src/main/java/ru/pipko/otus/homework/library/domain/Author.java
+++ library/src/main/java/ru/pipko/otus/homework/library/domain/Author.java
@@ -2,14 +2,23 @@ package ru.pipko.otus.homework.library.domain;
 
 import lombok.*;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "authors")
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
 public class Author {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id",nullable = false,unique = true)
     private Long id;
 
+    @Column(name = "name",unique = true)
     private String name;
 
     public Author(String name) {
--- library/src/main/java/ru/pipko/otus/homework/library/domain/Book.java
+++ library/src/main/java/ru/pipko/otus/homework/library/domain/Book.java
@@ -2,18 +2,30 @@ package ru.pipko.otus.homework.library.domain;
 
 import lombok.*;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "books")
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "name")
     private String name;
 
+    @OneToOne
+    @JoinColumn(name = "author_id")
     private Author author;
 
+    @OneToOne
+    @JoinColumn(name = "genre_id")
     private Genre genre;
 
     public Book(String name, Author author, Genre genre) {
