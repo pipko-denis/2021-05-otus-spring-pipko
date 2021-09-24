c1d9e302481f2a3395862780720ab491b93f58b2
pipko-denis <pipko_work@mail.ru>
2021-09-24 20:42:26 +0300

    AuthorJpaDao since now has transactional annotation

--- library/src/main/java/ru/pipko/otus/homework/library/dao/AuthorJpaDao.java
+++ library/src/main/java/ru/pipko/otus/homework/library/dao/AuthorJpaDao.java
@@ -2,6 +2,7 @@ package ru.pipko.otus.homework.library.dao;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.pipko.otus.homework.library.domain.Author;
 
 import javax.persistence.EntityManager;
@@ -21,28 +22,32 @@ public class AuthorJpaDao implements AuthorDao{
     }
 
 
-
+    @Transactional
     @Override
     public List<Author> getAll() {
         TypedQuery<Author> query = entityManager.createQuery("SELECT e FROM Author e order by e.name",Author.class);
         return query.getResultList();
     }
 
+    @Transactional
     @Override
     public Author getById(long id) {
         return null;
     }
 
+    @Transactional
     @Override
     public List<Author> getByName(String name) {
         return null;
     }
 
+    @Transactional
     @Override
     public int insert(Author author) {
         return 0;
     }
 
+    @Transactional
     @Override
     public int delete(long id) {
         return 0;
