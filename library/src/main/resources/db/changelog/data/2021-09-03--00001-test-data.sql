49a100f0a71f8cc8e496a4ed546d440628882ba6
pipko-denis <pipko_work@mail.ru>
2021-09-29 21:03:47 +0300

    Data for test modified

--- library/src/main/resources/db/changelog/data/2021-09-03--00001-test-data.sql
+++ library/src/main/resources/db/changelog/data/2021-09-03--00001-test-data.sql
@@ -13,3 +13,8 @@ INSERT INTO BOOKS(id,name,author_id, genre_id) VALUES(1, 'Book1',1,1);
 INSERT INTO BOOKS(id,name,author_id, genre_id) VALUES(2, 'Book2',1,2);
 INSERT INTO BOOKS(id,name,author_id, genre_id) VALUES(3, 'Book3',2,1);
 INSERT INTO BOOKS(id,name,author_id, genre_id) VALUES(4, 'Book4',2,2);
+
+--changeset pipko:2021-09-29--00001-all-data context:test
+INSERT INTO COMMENTS(id, book_id, text) VALUES(1, 1, 'Comment on book 1 #1');
+INSERT INTO COMMENTS(id, book_id, text) VALUES(2, 1, 'Comment on book 1 #2');
+INSERT INTO COMMENTS(id, book_id, text) VALUES(3, 2, 'Comment on book 2 #1');
