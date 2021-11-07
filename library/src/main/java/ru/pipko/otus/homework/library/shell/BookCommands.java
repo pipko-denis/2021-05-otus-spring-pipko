32828e24f237b9e51f44fe2ccb9ebcf2286ae139
Denis Pipko <pipko_work@mail.ru>
2021-11-08 02:05:53 +0300

    BookCommands since now use BookCommandsExecutorService instead of BooksEditorService, which gives correct output and work with transactions

--- library/src/main/java/ru/pipko/otus/homework/library/shell/BookCommands.java
+++ library/src/main/java/ru/pipko/otus/homework/library/shell/BookCommands.java
@@ -1,55 +1,32 @@
 package ru.pipko.otus.homework.library.shell;
 
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
-import org.springframework.transaction.annotation.Transactional;
-import ru.pipko.otus.homework.library.dao.BookDao;
-import ru.pipko.otus.homework.library.domain.Author;
-import ru.pipko.otus.homework.library.domain.Book;
-import ru.pipko.otus.homework.library.domain.Comment;
-import ru.pipko.otus.homework.library.domain.Genre;
-import ru.pipko.otus.homework.library.dto.BookComment;
-import ru.pipko.otus.homework.library.service.BooksEditorService;
-
-import java.util.List;
-import java.util.stream.Collectors;
+import ru.pipko.otus.homework.library.service.BookCommandsExecutorService;
 
 @ShellComponent
+@RequiredArgsConstructor
 public class BookCommands {
 
-    private final BooksEditorService booksEditorService;
-
-    private final BookDao bookDao;
-
-    private final int outCommentsCount;
-
-    public BookCommands(BooksEditorService booksEditorService, BookDao bookDao, @Value("${book.out.comments.count}") int outCommentsCount) {
-        this.booksEditorService = booksEditorService;
-        this.bookDao = bookDao;
-        this.outCommentsCount = outCommentsCount;
-    }
+    private final BookCommandsExecutorService commandsExecutorService;
 
 
-    @ShellMethod(value = "Adding book with all requisites, split list of authors and genres by comma", key = {"ba", "book-add"})
+    @ShellMethod(value = "Adding book with all requisites, split list of ids of authors and genres by comma", key = {"ba", "book-add"})
     public String addBook(@ShellOption(value = {"book", "b"}) String bookName,
                           @ShellOption(value = {"author", "a"}) String authors,
                           @ShellOption(value = {"genre", "g"}) String genreId) {
 
-        Book book = booksEditorService.addBook(bookName, authors, genreId);
+        return commandsExecutorService.addBook(bookName, authors, genreId);
 
-        return "Book added, id: " + book.getId() +
-                ". Authors: " + book.getAuthors().stream().map(Author::getName).collect(Collectors.joining(",")) +
-                ". Genres: " + book.getGenres().stream().map(Genre::getName).collect(Collectors.joining(","));
     }
 
     @ShellMethod(value = "Adding book with all requisites", key = {"bd", "book-del"})
     public String deleteBook(@ShellOption(value = {"id"}) String bookId) {
 
-        int recCount = booksEditorService.deleteBookById(bookId);
+        return commandsExecutorService.deleteBookById(bookId);
 
-        return "Book with id = " + bookId + " deleted from Books. Modified " + recCount + " rows.";
     }
 
     @ShellMethod(value = "Edit book's name, authors and genres, split list of authors and genres by comma", key = {"be", "book-edit"})
@@ -58,63 +35,30 @@ public class BookCommands {
                            @ShellOption(value = {"author", "a"}) String authorId,
                            @ShellOption(value = {"genre", "g"}) String genreId) {
 
-        Book book = booksEditorService.editBook(bookId, bookName, authorId, genreId);
+        return commandsExecutorService.editBook(bookId, bookName, authorId, genreId);
 
-        return "Book with id = " + book.getId() + " modified.";
     }
 
-    @Transactional
     @ShellMethod(value = "Listing all books", key = {"books-list", "bl"})
     public String getBooksList() {
 
-        final List<Book> bookList = booksEditorService.getAllBooks();
+        return commandsExecutorService.getAll();
 
-        final String result = "Books list: \n" + bookList.stream().map(book ->
-                "Book id:" + book.getId() + ": \"" + book.getName() + "\", " +
-                        "authors: " + book.getAuthors().stream().map(Author::getName).collect(Collectors.joining(",")) + ", " +
-                        "genre: " + book.getGenres().stream().map(Genre::getName).collect(Collectors.joining(",")) + ", " +
-                        "comments: " + book.getComments().stream()
-                        .map(comment -> getCommentPart(comment))
-                        .limit(outCommentsCount)
-                        .collect(Collectors.joining(",")) + " "
-        ).collect(Collectors.joining("\n"));
-
-        return result;
-    }
-
-    private String getCommentPart(Comment comment) {
-        String commentText = comment.getText();
-        if ((commentText != null) && (commentText.length() > 10)) {
-            return commentText.substring(0, 10) + "...";
-        } else {
-            return commentText;
-        }
     }
 
 
-    @Transactional
-    @ShellMethod(value = "All information about book", key = {"book-info", "bi"})
+    @ShellMethod(value = "All information about book by it's id", key = {"book-info", "bi"})
     public String getBook(@ShellOption(value = {"id"}) String bookId) {
 
-        final Book book = booksEditorService.getBookById(bookId);
+        return commandsExecutorService.getBookById(bookId);
 
-        final String result = "Book id: " + book.getId() + "\n" +
-                "Book name: \"" + book.getName() + "\".\n" +
-                "Authors: " + book.getAuthors().stream().map(Author::getName).collect(Collectors.joining(", ")) + "\n" +
-                "Genres: " + book.getGenres().stream().map(Genre::getName).collect(Collectors.joining(", ")) + "\n" +
-                "Comments: " + book.getComments().stream().map(Comment::getText).collect(Collectors.joining(", "));
-        return result;
     }
 
-    @Transactional
-    @ShellMethod(value = "Get books list with theirs comments count", key = {"book-comments-count", "bcc"})
-    public String getBookComments(@ShellOption(value = {"limit","l"}, defaultValue = "5") String limit) {
+    @ShellMethod(value = "Get books list with theirs comments count, you can set list limit", key = {"book-comments-count", "bcc"})
+    public String getBookComments(@ShellOption(value = {"limit","l"}, defaultValue = "5") Integer limit) {
 
-        final List<BookComment> bookComments = booksEditorService.getBookCommentsCnt(limit);
+        return commandsExecutorService.getBookCommentsCnt(limit.toString());
 
-        final String result = bookComments.stream().map(book ->
-                "Book name: \"" + book.getBookName() + ", comments count: " + book.getCommentsCount()).collect(Collectors.joining("\n"));
-        return result;
     }
 
 
