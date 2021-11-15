7553ee36f86c74f7499f3fd247a89bb288bea892
Denis Pipko <pipko_work@mail.ru>
2021-11-15 10:39:00 +0300

    New three types of exceptions

---
+++ library/src/main/java/ru/pipko/otus/homework/library/exceptions/DaoRuntimeException.java
@@ -0,0 +1,9 @@
+package ru.pipko.otus.homework.library.exceptions;
+
+public class DaoRuntimeException extends RuntimeException{
+
+
+    public DaoRuntimeException(String message) {
+        super(message);
+    }
+}
